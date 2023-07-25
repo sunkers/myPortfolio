0e0f71910390451098c8c3643f0ce16dce3d5356
Loic <mougin.loic@gmail.com>
2023-07-25 21:38:17 +0200

    Title

--- src/components/Titre.js
+++ src/components/Titre.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import {sd, richText, appear, iconContainer} from './Titre.module.scss'
 
-const Intro = () => {
-  return (
-    <section>
-      {/* Votre code pour l'introduction ici */}
-    </section>
-  )
+const Titre = () => {
+    return (
+        <div className={sd}>
+            <div className={`${sd} ${richText}`}>
+                <h1 style={{textTransform: "uppercase"}}>
+                    Web Developer Passion and <strong><u>EXPERIENCE.</u></strong>
+                </h1>
+            </div>
+            <div className={`${sd} ${appear}`}>
+                <div className={`${iconContainer}`}>
+                    <FontAwesomeIcon icon={faTwitter} />
+                </div>
+                <div className={`${iconContainer}`}>
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </div>
+                <div className={`${iconContainer}`}>
+                    <FontAwesomeIcon icon={faGithub} />
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default Intro
+export default Titre
