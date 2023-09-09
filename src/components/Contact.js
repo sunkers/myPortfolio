bcfba57c016502d8179e0570474440aa03c06c77
Loic <mougin.loic@gmail.com>
2023-09-09 14:24:28 +0200

    contact api

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,32 @@ import React from "react";
 import * as style from "./Contact.module.scss";
 
 const Contact = () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const sender = event.target.sender.value;
+    const email = event.target.email.value;
+    const message = event.target.message.value;
+
+    try {
+      const response = await fetch("http://localhost:3001/send-email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ sender, email, message }),
+      });
+
+      if (response.ok) {
+        console.log("Email envoyé avec succès");
+      } else {
+        console.log("Échec de l'envoi de l'email");
+      }
+    } catch (error) {
+      console.error("Il y a eu un problème avec l'envoi de l'email:", error);
+    }
+  };
+
   return (
     <div className={style.ContactContainer}>
       <div className={style.contactBox}>
@@ -9,35 +35,43 @@ const Contact = () => {
           <h2>CONTACT</h2>
           <div className={style.links}>
             <div className={style.link}>
-              <a>
+              <a
+                href="https://www.linkedin.com/in/lo%C3%AFc-mougin-184b4275/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <img src="/linkedin.png" alt="linkedin"></img>
               </a>
             </div>
             <div className={style.link}>
-              <a>
+              <a
+                href="https://github.com/sunkers"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <img src="/github.png" alt="github"></img>
               </a>
             </div>
             <div className={style.link}>
-              <a>
+              <a href="mailto:mougin.loic@gmail.com">
                 <img src="/mail.png" alt="email"></img>
               </a>
             </div>
           </div>
         </div>
         <div className={style.contactFormWrapper}>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className={style.formItem}>
-              <input type="text" name="sender" required />
-              <label>Name:</label>
+              <label htmlFor="sender">Name:</label>
+              <input type="text" name="sender" id="sender" required />
             </div>
             <div className={style.formItem}>
-              <input type="text" name="email" required />
-              <label>Email:</label>
+              <label htmlFor="email">Email:</label>
+              <input type="text" name="email" id="email" required />
             </div>
             <div className={style.formItem}>
-              <textarea name="message" required></textarea>
-              <label>Message:</label>
+              <label htmlFor="message">Message:</label>
+              <textarea name="message" id="message" required></textarea>
             </div>
             <button className={style.submitBtn}>Send</button>
           </form>
---
+++ src/services/server.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+
+// Utiliser CORS pour toutes les routes
+app.use(cors());
+
+// Middleware pour lire le JSON du corps de la requête
+app.use(express.json());
+
+app.post("/send-email", async (req, res) => {
+  const { sender, email, message } = req.body;
+
+  // Configurer le transporteur
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  // Configuration de l'e-mail
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: [process.env.EMAIL_USER],
+    subject: "Message du formulaire de contact",
+    text: `Nom: ${sender}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  // Envoyer l'e-mail
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).send("Email envoyé à l'administrateur");
+  } catch (error) {
+    console.error("Erreur lors de l'envoi de l'e-mail:", error);
+    res.status(500).send("Erreur interne du serveur");
+  }
+});
+
+// Lancer le serveur
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
